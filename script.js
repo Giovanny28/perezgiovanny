0dc1af07f67f0c1541a02e7287e880424be8555d
Giovanny28 <giovannypn28@gmail.com>
2025-09-25 18:44:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -20,15 +20,16 @@ const citas = [
 ];
 
 // Definimos la función que mostrará una cita aleatoria
-function generarCita() {
+function generarNombre() {
   // Generamos un número aleatorio entre 0 y la cantidad de frases (citas.length)
   // Math.random() → número entre 0 y 1
   // * citas.length → escala ese número al rango de frases disponibles
   // Math.floor() → redondea hacia abajo para obtener un número entero válido
-  const indice = Math.floor(Math.random() * citas.length);
+  const indice = Math.floor(Math.random() * nombre.length);
 
   // Buscamos en el documento el elemento con id="quote"
   // Cambiamos su contenido de texto por la frase que corresponde al índice generado
-  document.getElementById("quote").innerText = citas[indice];
+  document.getElementById("quote").innerText = nombre[indice];
 }
+
 
