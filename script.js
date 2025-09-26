7a22cf847ac70b113ea05f7150e7d2dd19338369
Giovanny28 <giovannypn28@gmail.com>
2025-09-25 22:08:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,12 @@ function generarCita() {
   // Cambiamos su contenido de texto por la frase que corresponde al índice generado
   document.getElementById("quote").innerText = citas[indice];
 }
+let contador = 0;
+function contarClics() {
+  contador++;
+  document.getElementById("quote").innerText = "Has hecho clic " + contador + " veces.";
+}
+
 
 
 
