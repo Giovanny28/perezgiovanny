20c9117915b42cec5f18cac6622e057d589bd23f
Giovanny28 <giovannypn28@gmail.com>
2025-09-25 18:59:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Declaramos un arreglo (array) con varias frases motivacionales
-const nombres = [
+const citas = [
   "Anakin Skywalker",
   "Obi Wan Kenobi",
   "Padme Amidala",
@@ -20,17 +20,18 @@ const nombres = [
 ];
 
 // Definimos la función que mostrará una cita aleatoria
-function generarNombre() {
+function generarCita() {
   // Generamos un número aleatorio entre 0 y la cantidad de frases (citas.length)
   // Math.random() → número entre 0 y 1
   // * citas.length → escala ese número al rango de frases disponibles
   // Math.floor() → redondea hacia abajo para obtener un número entero válido
-  const indice = Math.floor(Math.random() * nombre.length);
+  const indice = Math.floor(Math.random() * citas.length);
 
   // Buscamos en el documento el elemento con id="quote"
   // Cambiamos su contenido de texto por la frase que corresponde al índice generado
-  document.getElementById("quote").innerText = nombre[indice];
+  document.getElementById("quote").innerText = citas[indice];
 }
+
 
 
 
